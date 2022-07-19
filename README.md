e0df191b4397994f0cbb4d3d9c4bbb152e91c2be
dbiruk <dbiruk@invicro.com>
2022-07-19 23:17:24 +0300

    Initial commit - created a virtual env, created a test python script,checked code with flake8

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python_learning
+
+This repo will be used to upload python scripts, configs, that were created during the learning process
---
+++ first_script.py
@@ -0,0 +1,11 @@
+import os
+
+def soritngArray ():
+    a = 10
+    b = 15 
+    return print("the result is",a+b)
+
+    
+
+if __name__ == '__main__': 
+    soritngArray()
