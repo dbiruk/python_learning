10b229d81e2372d0405195cf14ab548bd83e5d6c
dbiruk <dbiruk@invicro.com>
2023-09-11 20:25:51 +0300

    fixed the quality check issue 09.11.2023

--- calculator.py
+++ calculator.py
@@ -7,51 +7,62 @@ operators = { "+": operator.add,
         '/' : operator.truediv, }
 
 def calculate (num1, num2, oper):
-    cil = calc_util.check_if_letters(num1, num2) 
-    if cil == False: stupid_input()
-    if oper == '/' and float(num2) == 0:
-        print("You can't divide by 0 moron")
-        stupid_input()   
-    
     res = calc_util.check_if_int(operators[oper](float(num1),float(num2)))
     print("The reuslt is: " + str(res))
+
+def quality_checks(num1, num2, oper):
+    cil = calc_util.check_if_letters(num1, num2) 
+    if cil == False: return False
+    if oper == '/' and float(num2) == 0:
+        print("You can't divide by 0 moron")
+        return False
 
 def single_input():
     one_input = (input("Input the operation: "))
     for x in operators:
         if x in one_input:
-            vals = one_input.split(x)
-            strip_vals = [s.strip() for s in vals]
-            strip_vals.append(x)
-    calculate(strip_vals[0], strip_vals[1], strip_vals[2])
+            raw_vals = one_input.split(x)
+            vals = [s.strip() for s in raw_vals]
+            vals.append(x)
+
+    return vals
 
 def multiple_inputs():
     num1 = input("The First Number:")
     oper = input("Operator:")
     num2 = input("The Second Number:")
-    calculate(num1, num2, oper)
+    vals = [num1, num2, oper]
+
+    return vals
 
 def stupid_input():
     maybe_stupid = input("=====================\n"+
         "Are you stoopid? Do you want to try again(y/n): ")
     if maybe_stupid == 'y':
-        input_choice()
+        return input_choice()
     elif maybe_stupid =='n':
         exit()
     else:
         stupid_input()
-
 
 def input_choice():
     choice = (input("Do you want to input one by one, or in a single line? \n"+
         "Type \"one\" to input one by one\n"+
         "Type \"single\" to input a single line: "))
     if choice == "one":
-        multiple_inputs()
+        return multiple_inputs()
     elif choice == "single":
-        single_input()
+        return single_input()
     else:
         stupid_input()
 
 
-input_choice()
+def main():
+    vals = input_choice()
+    if quality_checks(vals[0], vals[1], vals[2]) == False:
+        vals = stupid_input()
+    calculate(vals[0], vals[1], vals[2])
+    
+
+if __name__ == '__main__':
+    main()
